9e47bea6c3efab67b6dacc7dc3403303b8a82686
noe <noe.breton@etu.univ-tours.fr>
2023-03-07 10:32:34 +0100

    zadazdazd

---
+++ app/src/main/java/com/example/wantedregistery/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.wantedregistery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //test
+    }
+}
