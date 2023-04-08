b53d0236d9a7dc82d39442678f155d41cd088063
noe <noe.breton@etu.univ-tours.fr>
2023-04-08 22:09:43 +0200

    add email and phone number to the contact card

--- app/src/main/java/Activity/ContactActivity.java
+++ app/src/main/java/Activity/ContactActivity.java
@@ -16,6 +16,10 @@ public class ContactActivity extends AppCompatActivity {
     private SharedPreferences sharedPref;
     private EditText name;
     private EditText lastName;
+    private EditText email;
+
+    private EditText phone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +29,23 @@ public class ContactActivity extends AppCompatActivity {
         sharedPref = this.getPreferences(Context.MODE_PRIVATE);
         name = (EditText) findViewById(R.id.nameEditText);
         lastName = (EditText) findViewById(R.id.lastNameEditText);
+        email = (EditText) findViewById(R.id.emailEditText);
+        phone = (EditText) findViewById(R.id.phoneEditText);
 
         name.setText(sharedPref.getString("name", ""));
         lastName.setText(sharedPref.getString("lastName", ""));
+        email.setText(sharedPref.getString("email", ""));
+        phone.setText(sharedPref.getString("phone", ""));
+
+
     }
 
     public void saveData(View v) {
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putString("name", name.getText().toString());
         editor.putString("lastName", lastName.getText().toString());
+        editor.putString("email", email.getText().toString());
+        editor.putString("phone", phone.getText().toString());
         editor.apply();
 
         Intent i = new Intent(this, WantedRecyclerActivity.class);
